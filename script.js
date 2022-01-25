b047ba3cdfeed9e60ffac217b42a913536a31029
Guillaume Macbook <g.dharcourt+github@posteo.net>
2022-01-26 00:04:43 +0100

    Ajout de la fonction complete "game", à débugger

--- script.js
+++ script.js
@@ -2,9 +2,6 @@
 const rock = 100 / 3;
 const paper = rock * 2;
 const scissors = rock * 3
-    // console.log(Rock);
-    // console.log(Paper);
-    // console.log(Scissors);
 
 function computerPlay() {
     // Génération de la variable du choix de l'ordinateur pour le tour 
@@ -25,28 +22,59 @@ const playerSelection = prompt("Rock, paper or scissors ?").toLowerCase();
 const computeurSelection = computerPlay();
 // const computeurSelection = "paper";
 
+const playerWin = "You win  !";
+const playerLoose = "You loose this time";
+const draw = "Draw"
+let result = "";
+
 console.log(`player choice is ${playerSelection}, computer choice is ${computeurSelection}`);
 
 function playRound(playerSelection, computeurSelection) {
-    if ((computeurSelection === "rock") && (playerSelection === "paper")) {
-        return `You win, ${playerSelection} beat ${computeurSelection}`;
-    } else if ((computeurSelection === "rock") && (playerSelection === "scissors")) {
-        return `You loose, ${playerSelection} is beaten by ${computeurSelection}`;
-    } else if ((computeurSelection === "paper") && (playerSelection === "scissors")) {
-        return `You win, ${playerSelection} beat ${computeurSelection}`;
-    } else if ((computeurSelection === "paper") && (playerSelection === "rock")) {
-        return `You loose, ${playerSelection} is beaten by ${computeurSelection}` ;
-    } else if ((computeurSelection === "scissors") && (playerSelection === "paper")) {
-        return `You loose, ${playerSelection} is beaten by ${computeurSelection}` ;   
-    } else if ((computeurSelection === "scissors") && (playerSelection === "rock")) {
-        return `You win, ${playerSelection} beat ${computeurSelection}`;
+    if (playerSelection === "rock" || playerSelection === "paper" || playerSelection === "scissors") {
+        if ((computeurSelection === "rock") && (playerSelection === "paper")) {
+            return playerWin;
+        } else if ((computeurSelection === "rock") && (playerSelection === "scissors")) {
+            return playerLoose;
+        } else if ((computeurSelection === "paper") && (playerSelection === "scissors")) {
+            return playerWin;
+        } else if ((computeurSelection === "paper") && (playerSelection === "rock")) {
+            return playerLoose;
+        } else if ((computeurSelection === "scissors") && (playerSelection === "paper")) {
+            return playerLoose;
+        } else if ((computeurSelection === "scissors") && (playerSelection === "rock")) {
+            return playerWin;
+        } else {
+            return draw;
+        }
     } else {
-        return `${playerSelection} vs ${computeurSelection}, Draw`
+        alert("Please enter a correct value");
     }
 }
 // console.log(playRound(playerSelection, computeurSelection));
-function game(nb) {
-    for (let i=0; i<nb; i++) {
-        playRound(playerSelection, computeurSelection)
+
+function game(numberOfRound) {
+    for (let i = 0; i <= numberOfRound; i++) {
+        let loss = 0;
+        let win = 0;
+        let tie = 0;
+        playRound(playerSelection, computeurSelection);
+        if (playRound = playerWin) {
+            win = win + 1;
+        } else if (playRound = playerLoose) {
+            loss = loss + 1;
+        } else {
+            tie = tie + 1;
+        }
+        console.log(`win : ${win}, loss : ${loss}, draw : ${tie}.`)
     }
+    if (loss < win) {
+        console.log(`${win} VS ${loss}, You loose.`)
+    } else if (loss > win) {
+        console.log(`${win} VS ${loss}, You win !.`)
+    } else {
+        console.log(`${win} VS ${loss}, tie !`)
+    }
+
 }
+
+game(prompt(""));
